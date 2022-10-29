78eb091d80ab817eb26df63b2dbd3610958ca0aa
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2022-10-29 16:41:46 +0530

    add error handlers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const apiRouter = require("./api");
+router.use("/api", apiRouter);/*All the URLs of the routes in the api router
+will be prefixed with /api.*/
+
 router.get("/hello/world", function (req, res) {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");

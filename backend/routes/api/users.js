46d0a584e730a0f70cd471c8193ab33c1e3aab22
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2022-11-12 02:02:37 +0530

    update validation for sign up & log in

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,10 +2,30 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const{ check } = require('express-validator');
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateSignup=[
+check('email')
+.exists({checkFalsy:true})
+.isEmail()
+.withMessage('Please provide a valid email.'),
+check('username')
+.exists({checkFalsy:true})
+.isLength({min:4})
+.withMessage('Please provide a username with at least 4 characters.'),
+check('password')
+.exists({checkFalsy:true})
+.isLength({min:6})
+.withMessage('Please provide a password with at least 6 characters.'),
+handleValidationErrors
+]
+
 const router = express.Router();
 // Sign up
 router.post(
   '/',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
